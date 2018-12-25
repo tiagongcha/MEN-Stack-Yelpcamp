38116fd7d6b84cbcf4c88d87f4c6622c7179b6fe
tiagongcha <tiagong25@gmail.com>
2018-12-24 21:19:28 -0700

    add database and showMoreInfo base for each object in the campDB

--- app.js
+++ app.js
@@ -1,44 +1,94 @@
 var express = require("express");
 var app = express();
 var bodyParser = require("body-parser");
-app.use(bodyParser.urlencoded({extended:true}));
+var mongoose = require("mongoose");
 
+//create yelpcamp database inside mongodb:
+mongoose.connect("mongodb://localhost/yelp_camp");
+//check if connection is successful or not:
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  console.log("we're connected");
+});
+
+
+app.use(bodyParser.urlencoded({extended:true}));
 app.set("view engine", "ejs");
+
+//schema setup:
+var campgroundSchema = new mongoose.Schema({
+	name: String,
+	img: String,
+	description: String
+});
+
+var Campground = mongoose.model("Campground", campgroundSchema);
+Campground.create(
+	{name:"Granite Hill", 
+	img: "https://pixabay.com/get/e835b20e29f0003ed1584d05fb1d4e97e07ee3d21cac104491f5c779a4edbdbd_340.jpg",
+	description: "a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur"	
+	},
+	function(err, campground){
+		if(err){
+			console.log("err");
+		}else{
+			console.log(campground);
+		}
+	});
+
+
 //get landing page route:
 app.get("/", function(req, res){
 	res.render("landing");
 });
 
-var campgrounds = [
-		{name:"hi", img: "https://pixabay.com/get/e837b1072af4003ed1584d05fb1d4e97e07ee3d21cac104491f5c471a0efb5bd_340.jpg"},
-		{name:"hello", img:"https://pixabay.com/get/e03db50f2af41c22d2524518b7444795ea76e5d004b0144590f5c97ea5edb0_340.jpg"},
-		{name:"hello", img:"https://pixabay.com/get/e03db50f2af41c22d2524518b7444795ea76e5d004b0144590f5c97ea5edb0_340.jpg"},
-		{name:"hello", img:"https://pixabay.com/get/e03db50f2af41c22d2524518b7444795ea76e5d004b0144590f5c97ea5edb0_340.jpg"},
-		{name:"hello", img:"https://pixabay.com/get/e03db50f2af41c22d2524518b7444795ea76e5d004b0144590f5c97ea5edb0_340.jpg"}
-	];
-
-//get the campground page listing all camp grounds
+//INDEX - get the campground page listing all camp grounds
 app.get("/campground", function(req, res){
-	//list will be replaced by database:
-	
-
-	res.render("campgrounds", {campgrounds: campgrounds});
+	//Get all campground from db and render that file:
+	Campground.find({}, function(err, allCampgrounds){
+		if(err){
+			console.log(err);
+		}else{
+			res.render("index", {campgrounds: allCampgrounds});
+		}
+	})
 });
 
+//render the form page:
 app.get("/campground/newCamp", function(req,res){
 	res.render("newCamp");
 });
 
-
+//post client's input to campground page:
 app.post("/campground", function(req, res){
 	//get the new campground data from the form and append it to the campground list/DB:
 	var name = req.body.name;
-	var img = req.body.imge;
-	// console.log(name);
-	// console.log();
-	var newCampground = {name:name, img:img};
-	campgrounds.push(newCampground);
-	res.redirect("/campground");
+	var img = req.body.img;
+	var description = req.body.description;
+	var newCampground = {name:name, img:img, description: description};
+	//create a new Campground object and save it to database:
+	Campground.create(newCampground, function(err, newCampObj){
+		if(err){
+			console.log("user input form item is wrong, cant be saved in database");
+		}else{
+			//if successfully saved in DB, redirect user to background
+			res.redirect("/campground");
+		}
+	});
+});
+
+//show one object in db:
+app.get("/campground/:id", function(req,res){
+	//find the campground with provided ID
+	Campground.findById(req.params.id, function(err, thiscamp){
+		if (err) {
+			console.log(err);
+		}else{
+			//render show template with that campground
+			res.render("show", {campground: thiscamp});
+		}
+	});
 });
 
 app.listen(3000, ()=>{
